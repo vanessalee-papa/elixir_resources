e69bd203667f3757e7e733bc51b54f0918cd3d01
Vanessa Lee at Papa <vlee@joinpapa.com>
2023-04-10 16:45:43 -0500

    Create books.ex

---
+++ notebooks/books.ex
@@ -0,0 +1,26 @@
+# Books
+
+## Elixir, Erlang, Beam, OTP
+
+* Testing Elixir (2021)
+* Designing Elixir Systems (2019)
+* Adopting Elixir (2019)
+* Craft GraphQL APIs in Eliixr (2018)
+* Metaprogramming Elixir (2015)
+* [Elixir in Action (2015)](https://elixirforum.com/t/8-years-of-elixir-books-which-ones-have-been-your-faves/45973O)
+* [Programming Elixir ≥ 1.6: Functional |> Concurrent |> Pragmatic |> Fun (2014)](https://www.amazon.com/Programming-Elixir-1-6-Functional-Concurrent/dp/1680502999)
+* Learn you some Erlang for Great Good (2013)
+
+## Ecto
+
+* [Programming Ecto (2019)](https://www.amazon.com/Programming-Elixir-1-6-Functional-Concurrent/dp/1680502999)
+
+## Phoenix
+
+* Phoenix in Action (2019)
+* Programming Phoenix (2016)
+
+## Functional Programming, General
+
+* [The Pragmatic Programmer (1999)](https://www.amazon.com/Pragmatic-Programmer-Journeyman-Master/dp/020161622X)
+* [The Little Schemer (1995)](https://www.amazon.com/Little-Schemer-Daniel-P-Friedman/dp/0262560992)
